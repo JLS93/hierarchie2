3a3a859754b387fefe3979eab36a20840c0703fc
JLS93 <jlsvbn@gmail.com>
2022-06-10 12:08:19 +0200

    Ajoutez des fichiers projet.

---
+++ hierarchie2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hierarchie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fichier = File.ReadAllLines(@"C:\Users\Administrateur\source\repos\cars\cars\BorneChargeVoitureElectrique.csv");
+        }
+    }
+}
